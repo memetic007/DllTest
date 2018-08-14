c1c5be6c2b40959c287c9b7b679332831b00392e
Jim Rutt <jimrutt@gmail.com>
2018-08-14 14:18:39 -0400

    Add project files.

---
+++ DllTest/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyDLL;
+
+
+
+
+namespace DllTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int aValue = MyDLLClass.Adder(10, 10);
+
+            button3.Text = aValue.ToString();
+
+        }
+    }
+}
---
+++ MyDLL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDLL
+{
+    public class MyDLLClass
+    {
+        public static int Adder(int a,int b)
+        {
+            int returnValue;
+            returnValue = a + b;
+            return returnValue;
+
+        }
+    }
+}
